7c2559d931c270cc6a57597b860b656de8659e08
Kavin <kavinp622@gmail.com>
2026-08-09 14:34:45 -0400

    About header edit

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -20,7 +20,7 @@ const AboutPage = () => {
     <section id="about" className="about-me">
       <PageHeader
         kicker="About"
-        heading="Precision from finance, insight from engineering."
+        heading="Insight from finance, precision from engineering."
       />
 
       <div className="about-me__content">
