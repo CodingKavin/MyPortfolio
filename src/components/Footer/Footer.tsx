df279952bf579470b57daeb8c3130dd6146b1e00
Kavin <kavinp622@gmail.com>
2026-08-09 13:36:56 -0400

    redesign footer and button components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,17 @@
-import Typography from "../Typography/Typography";
 import Icon from "../Iconography/Iconography";
 import "./Footer.scss";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <Typography variant="p3" className="footer__copy">
-        &copy; Made by Kavin Paul.
-      </Typography>
+      <span className="footer__copy">&copy; Made by Kavin Paul.</span>
       <div className="footer__socials">
         <a
           href="https://github.com/CodingKavin"
           target="_blank"
           rel="noopener noreferrer"
+          aria-label="GitHub"
+          title="GitHub"
           className="footer__link"
         >
           <Icon name="github" />
@@ -21,6 +20,8 @@ const Footer = () => {
           href="https://www.linkedin.com/in/kavin-paul-dev/"
           target="_blank"
           rel="noopener noreferrer"
+          aria-label="LinkedIn"
+          title="LinkedIn"
           className="footer__link"
         >
           <Icon name="linkedin" />
